310b728037ef824a02496f149a9653017381626a
Sai Alekya <saialekyag@gmail.com>
2023-06-12 15:54:13 -0500

    Success

---
+++ my-portfolio/src/data.js
@@ -0,0 +1,68 @@
+export const projects = [
+  // {
+  //   title: "React Reserve",
+  //   subtitle: "MERN Stack",
+  //   description:
+  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+  //   image: "./project-1.gif",
+  //   link: "https://reactbootcamp.com",
+  // },
+  // {
+  //   title: "React Tracks",
+  //   subtitle: "React and Python",
+  //   description:
+  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+  //   image: "./project-2.gif",
+  //   link: "https://reedbarger.com",
+  // },
+  {
+    title: "SocialMedia Dashboard",
+    subtitle: "React",
+    description:
+      "The social media dashboard with a theme switcher is a dynamic web application built with HTML, CSS, and JavaScript. It allows users to manage their social media accounts and switch between different visual themes. The dashboard displays real-time social media metrics and provides features like task management. It offers a personalized and visually appealing theme switcher experience for effective social media management using dashboard and analysis.",
+    image: "./social_media_demo1.gif",
+    link: "https://jsbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "The ReactJS Todo App is a simple yet powerful task management application. With this app, users can effortlessly add and delete tasks to keep track of their to-do lists. The intuitive user interface allows for quick and seamless task entry, while the delete function ensures easy removal of completed or unnecessary tasks. This minimalist and efficient ReactJS Todo App provides a streamlined solution for organizing and managing tasks effectively.",
+    image: "./todo_app_demo.gif",
+    link: "https://pythonbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "TCS Gems award for outstanding performance and contribution to project goals and Service Commitment Award for dedication and commitment to high-quality service.",
+    // image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "INTERACTIVE",
+    company: "TCS",
+  },
+  {
+    quote:
+      "	MFDM™ AI Aware Badge for proficiency in artificial intelligence and machine learning technologies , Agile Certification for proficiency in agile methodologies.",
+    // image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "INTERACTIVE",
+    company: "TCS",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+  "Angular 2/4/6/7/9/11",
+  "Bootstrap",
+  "Karma, Jasmine, Developer Tools",
+  "Agile, Jira, Rally, scrum",
+  "My SQL, Mongo DB, SQL server",
+  "Git, Bitbucket",
+  "Visual Studio, Eclipse, Eclipse IDE, Firebug, Developer Tools",
+  "AWS, GCP developing Client/Server"
+];
